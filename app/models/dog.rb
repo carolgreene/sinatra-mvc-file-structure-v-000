c59582d7a9d86285ab32268539e89ce9ec95bce5
Carol Greene <carolgreene3@gmail.com>
2018-03-25 14:41:44 +0000

    automatically backed up by learn

--- app/models/dog.rb
+++ app/models/dog.rb
@@ -1,16 +1,16 @@
  class Dog
-   attr_accessor :name, :breed, :age 
+   attr_accessor :name, :breed, :age
    @@all = []
 
    def initalize(name, breed, age)
-     @name = name 
-     @breed = breed 
-     @age = age 
+     @name = name
+     @breed = breed
+     @age = age
      @@all < self
-   end 
+   end
 
-   def self.all 
-     @@all 
+   def self.all
+     @@all
    end
 
  end
